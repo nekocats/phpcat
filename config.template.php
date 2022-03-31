63dc544d37f262c6512b15fc99a3cbe14238e018
d3adcat <mynameismgamer@gmail.com>
2022-03-31 09:54:46 +0300

    more  stuff ig

---
+++ config.template.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'db' => 'books',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ]
+];
